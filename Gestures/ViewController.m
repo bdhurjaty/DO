2ab0a8226adb2b602b3414deef354d50c3f44bc6
Balasubrahmanyam Dhurjaty <dhurjatybs@gmail.com>
2016-10-02 00:12:59 +0530

    swipe gesture
    swipe gesture

--- Gestures/ViewController.m
+++ Gestures/ViewController.m
@@ -9,6 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    UIView *view1;
+}
+-(void)rightSwipeMethod:(UISwipeGestureRecognizer *)gesture;
+-(void)leftSwipeMethod:(UISwipeGestureRecognizer *)gesture;
 
 @end
 
@@ -17,6 +22,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    UISwipeGestureRecognizer *rightSwipe =[[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(rightSwipeMethod:)];
+    UISwipeGestureRecognizer *leftSwipe =[[UISwipeGestureRecognizer alloc] initWithTarget:self action: @selector(leftSwipeMethod:)];
+    
+    rightSwipe.direction = UISwipeGestureRecognizerDirectionRight;
+    leftSwipe.direction = UISwipeGestureRecognizerDirectionLeft;
+    
+//    [self.view addGestureRecognizer:rightSwipe];
+//    [self.view addGestureRecognizer:leftSwipe];
+    
+    view1 = [[UIView alloc]initWithFrame:CGRectMake(100, 100, 200, 200)];
+    view1.backgroundColor = [UIColor blackColor];
+    
+    [self.view addSubview:view1];
+    
+    [view1 addGestureRecognizer:rightSwipe];
+    [view1 addGestureRecognizer:leftSwipe];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +46,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)rightSwipeMethod:(UISwipeGestureRecognizer *)gesture{
+    self.view.backgroundColor = [UIColor redColor];
+    view1.backgroundColor = [UIColor greenColor];
+}
+-(void)leftSwipeMethod:(UISwipeGestureRecognizer *)gesture{
+    self.view.backgroundColor = [UIColor greenColor];
+    view1.backgroundColor = [UIColor redColor];
+}
 @end
+
