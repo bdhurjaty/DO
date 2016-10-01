8f097299efc24ac1f6073cc24eb3d7f4879d4574
Balasubrahmanyam Dhurjaty <bdhurjaty@miadmins-macbook-pro.local>
2016-10-01 23:14:17 +0530

    Initial Commit

---
+++ Gestures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gestures
+//
+//  Created by Balasubrahmanyam Dhurjaty on 01/10/16.
+//  Copyright © 2016 Balasubrahmanyam Dhurjaty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gestures/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gestures
+//
+//  Created by Balasubrahmanyam Dhurjaty on 01/10/16.
+//  Copyright © 2016 Balasubrahmanyam Dhurjaty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
